8aa4936af053b0c88c876d9559ba9f66c8f84749
andy72039 <andy72039@gmail.com>
2016-05-07 11:23:22 +0800

    create background sound but no import minim

--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -7,18 +7,23 @@ import processing.core.PApplet;
 * You can do major UI control and some visualization in this class.  
 */
 @SuppressWarnings("serial")
+
 public class MainApplet extends PApplet{
 	private String path = "main/resources/";
 	private String file = "starwars-episode-1-interactions.json";
 	
 	private final static int width = 1200, height = 650;
+	Minim minim;
+	AudioPlayer song;
 	
 	public void setup() {
 
 		size(width, height);
 		smooth();
 		loadData();
-		
+		minim = new Minim(this);
+		song = minim.loadFile(this.getClass().getResource("/res/star_wars.mp3").getPath());
+		song.play();
 	}
 
 	public void draw() {
